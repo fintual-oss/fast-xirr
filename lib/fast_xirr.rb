1449bbdb259a97510d2c3224b82a66aeccb5fae9
Jaime Castro <5043042+jecastro1@users.noreply.github.com>
2024-07-03 12:56:41 -0400

    refactor: sort cashflows by timestamp in calculate method

--- lib/fast_xirr.rb
+++ lib/fast_xirr.rb
@@ -4,7 +4,8 @@ module FastXirr
   def self.calculate(cashflows:, tol: 1e-7, max_iter: 1e10)
     cashflows_with_timestamps = cashflows.map do |amount, date|
       [amount, Time.utc(date.year, date.month, date.day).to_i]
-    end
+    end.sort_by { |_amount, timestamp| timestamp }
+
     result = _calculate_with_brent(cashflows_with_timestamps, tol, max_iter)
     
     if result.nan?
